ceec4fb39bc80e04f0b7355f06f9c8f5a72b9cda
unknown <andrzej.gauza@outlook.com>
2024-05-30 17:39:01 +0200

    Add moving player to room center

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private InputActionReference teleportAction;
 
+    private CharacterControllerDriver characterControllerDriver;
+    private TeleportationProvider teleportationProvider;
+
     private void Awake()
     {
         teleportAction.action.performed += OnTeleportAction;
+
+        characterControllerDriver = GetComponent<CharacterControllerDriver>();
+        teleportationProvider = GetComponentInChildren<TeleportationProvider>();
     }
 
     private void OnTeleportAction(InputAction.CallbackContext _)
@@ -21,6 +28,8 @@ public class PlayerController : MonoBehaviour
     private void TeleportPlayer()
     {
         RoomsManager.Instance.ChangeRoom();
-        Debug.Log("teleport");
+        TeleportRequest teleportRequest = new TeleportRequest();
+        teleportRequest.destinationPosition = Vector3.zero;
+        teleportationProvider.QueueTeleportRequest(teleportRequest);
     }
 }
